3ecce157e6d2b552386acd34199b62bd2f8e2990
planegy <planegy.de@gmail.com>
2025-06-21 02:18:25 +0200

    Create linkedin_bot.py

---
+++ linkedin_bot.py
@@ -0,0 +1,15 @@
+import openai, os, requests
+from datetime import datetime
+import random
+
+HASHTAGS = ["#KommunaleWärmeplanung","#DezentraleEnergie","#ESG","#Energieeffizienz","#SmartCity"]
+
+class LinkedInContentAgent:
+    def __init__(...):
+        ...
+
+    def generate_content(...):
+        ...
+
+    def post_to_linkedin(...):
+        ...
